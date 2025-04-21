12de738cb025b034a5227533e269ab65aa42451d
karanpreet <karanpreet@orientaloutsourcing.com>
2025-04-21 13:56:40 +0530

    contact capta changes

---
+++ src/config/constants.js
@@ -0,0 +1,8 @@
+// const BASE_URL_LIVE = 'http://localhost:5000';
+const BASE_URL_LIVE          = 'https://customcodedweb-api.orientaloutsourcing.com';
+const CAPTCHA_SECRET_KEY     = '6LcIqBorAAAAABkAhVjH1fpaIWu3Lsew6SRZbi2g';
+
+module.exports = {
+  BASE_URL_LIVE,
+  CAPTCHA_SECRET_KEY
+};
